9820ffaa2be27ad45564b555e347053d30b5dd66
selvakumar V.R.S <157942095+Selvamaxy@users.noreply.github.com>
2026-08-09 19:30:49 +0530

    Time: 56 ms (5.53%), Space: 47 MB (6.07%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> st=new Stack<>();
+        for(String w:tokens){
+            if(w.equals("+"))
+                st.push(st.pop()+st.pop());
+            else if(w.equals("*"))
+                st.push(st.pop()*st.pop());
+            else if(w.equals("/")){
+            int n=st.pop();
+                st.push(st.pop()/n);}
+            else if(w.equals("-")){
+            int n=st.pop();
+                st.push(st.pop()-n);}
+            else
+            {
+                st.push(Integer.parseInt(w));
+            }
+            System.out.println(st.peek());
+        }
+        return st.pop();
+    }
+}
