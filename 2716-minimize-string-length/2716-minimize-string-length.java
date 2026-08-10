ced4ad666a0f84b107ebb3af020d74ebe138cc4b
selvakumar V.R.S <157942095+Selvamaxy@users.noreply.github.com>
2026-08-10 20:23:36 +0530

    Time: 3 ms (99.91%), Space: 46.5 MB (84.77%) - LeetHub

--- 2716-minimize-string-length/2716-minimize-string-length.java
+++ 2716-minimize-string-length/2716-minimize-string-length.java
@@ -1,14 +1,13 @@
 class Solution {
     public int minimizedStringLength(String s) {
-        int found=0;
         boolean[] arr=new boolean[26];
+        int count=0;
         for(int i=0;i<s.length();i++){
             if(arr[s.charAt(i)-'a']!=true){
-            arr[(s.charAt(i)-'a')]=true;
-            found++;
+                arr[s.charAt(i)-'a']=true;
+                count++;
             }
         }
-      
-        return found;
+        return count;
     }
 }
