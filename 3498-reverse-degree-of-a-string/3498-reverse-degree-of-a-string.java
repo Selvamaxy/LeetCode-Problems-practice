e0af53f78d4d26c846c042e6a204467b0cf6b6f8
selvakumar V.R.S <157942095+Selvamaxy@users.noreply.github.com>
2026-09-20 20:18:48 +0530

    Time: 1 ms (100%), Space: 44.1 MB (34.29%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -1,13 +1,10 @@
 class Solution {
     public int reverseDegree(String s) {
-        int sum=0;
-        int iter=0;
-        for(char c:s.toCharArray()){
-            iter+=1;
-            int num=c-'a'+1;
-            sum+=iter*(27-num);
-            //System.out.printf("%d %d ",num,sum);
-        }
-        return sum;
+     int tot=0;
+     for(int i=0;i<s.length();i++){
+        tot+=(int)('z'-s.charAt(i)+1)*(i+1);
+       // System.out.println(tot);
+     }
+     return tot;
     }
 }
